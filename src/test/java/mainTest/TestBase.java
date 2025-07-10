e06a0f472df484d54ddeac1bf2fd8c8d493167ce
Danil <dan.pon23@mail.ru>
2025-07-10 11:07:33 +0300

    something fix for allure

--- src/test/java/mainTest/TestBase.java
+++ src/test/java/mainTest/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
     @BeforeAll
     static void setup() {
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("base_size","1920x1080") ;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
